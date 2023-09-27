a99eb62bccf61d5fb94b15eec2bcf52152d9e297
Carlo Bonandrini <carlobonandrini@Carlos-MacBook-Pro.local>
2023-09-27 11:38:52 +0200

    restructuring of the plugins

---
+++ plugins/git.lua
@@ -0,0 +1,24 @@
+---@type NvPluginSpec[]
+local plugins = {
+  {
+    "lewis6991/gitsigns.nvim",
+    dependencies = {
+      {
+        "sindrets/diffview.nvim",
+        config = true,
+      },
+    },
+  },
+  {
+    "NeogitOrg/neogit",
+    cmd = "Neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+      "sindrets/diffview.nvim",
+    },
+    config = true,
+  },
+}
+
+return plugins
